0696b4cd0022d556c344ca97128f9efe1d6d8185
NinjaTomate <ninjatomate@gmail.com>
2014-01-25 15:49:34 +0100

    Added a moodule.

---
+++ modules/moo.py
@@ -0,0 +1,15 @@
+import variables, urllib2, string, subprocess
+def moo(send_data, msgarr, user):
+	message=""
+	msghold=""
+	for word in msgarr:
+		print word
+		if not (word == "") and not (word == ".moo"):
+			message+= word + " "
+	message=string.split(message, ".moo")
+	for word in message:
+		msghold+=word
+	message=msghold
+	output=subprocess.Popen(["cowsay", message],  stdout=subprocess.PIPE)
+    for PythonIsGreat in output.stdout:
+    	send_data("PRIVMSG %s :%s" % (CHANNEL, PythonIsGreat))
