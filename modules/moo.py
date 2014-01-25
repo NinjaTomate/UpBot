100d7250c1eab9ec69755426c65058c9924c23c0
NinjaTomate <ninjatomate@gmail.com>
2014-01-25 16:07:37 +0100

    Updated moo.py

--- modules/moo.py
+++ modules/moo.py
@@ -4,7 +4,7 @@ def moo(send_data, msgarr, user):
 	msghold=""
 	for word in msgarr:
 		print word
-		if not (word == "") and not (word == ".moo"):
+		if not (word == "") and not (word == ".moo") and not ("&" in word):
 			message+= word + " "
 	message=string.split(message, ".moo")
 	for word in message:
