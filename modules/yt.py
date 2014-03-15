7e329a141a6a3837680abcdcf0f3d8a28b34e2b2
NinjaTomate <ninjatomate@gmail.com>
2014-03-15 18:20:06 +0100

    Added YouTube module.

---
+++ modules/yt.py
@@ -0,0 +1,13 @@
+import variables, urllib, json as simplejson, string, HTMLParser
+def yt(send_data, msgarr, user):
+	if len(msgarr) < 2 in msgarr:
+		send_data("PRIVMSG %s :No or invalid search string supplied." % variables.channel)
+	else:
+		parser = HTMLParser.HTMLParser()
+		query=string.join(msgarr)[3:]
+		url = "https://gdata.youtube.com/feeds/api/videos?q=%s&alt=json" % query
+		print query
+		json = simplejson.load(urllib.urlopen(url))
+		title = parser.unescape(json['feed']['entry'][0]['link'][0]['href'])
+		url = parser.unescape(json['feed']['entry'][0]['title']['$t']
+		send_data("PRIVMSG %s :%s - %s - %s" % (variables.channel, title, content, url))
