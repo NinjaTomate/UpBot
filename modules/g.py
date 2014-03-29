9b4ef3e3dc97181b5cd773b3bd6b9ee1a558950e
NinjaTomate <ninjatomate@gmail.com>
2014-03-30 00:34:00 +0100

    Fixed Google Search crash.

--- modules/g.py
+++ modules/g.py
@@ -3,12 +3,15 @@ def g(send_data, msgarr, user):
 	if len(msgarr) < 2 or "porn" in msgarr:
 		send_data("PRIVMSG %s :No or invalid search string supplied." % variables.channel)
 	else:
-		parser = HTMLParser.HTMLParser()
-		query=string.join(msgarr)[3:]
-		url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s" % query
-		print query
-		json = simplejson.load(urllib.urlopen(url))
-		content = parser.unescape(json['responseData']['results'][0]['content']).replace("\n", "").replace("<b>", "").replace("</b>", "")
-		title = parser.unescape(json['responseData']['results'][0]['title']).replace("<b>", "").replace("</b>", "")
-		url = json['responseData']['results'][0]['unescapedUrl']
-		send_data("PRIVMSG %s :%s - %s - %s" % (variables.channel, title, content, url))
+		try:
+			parser = HTMLParser.HTMLParser()
+			query=string.join(msgarr)[3:]
+			url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s" % query
+			print query
+			json = simplejson.load(urllib.urlopen(url))
+			content = parser.unescape(json['responseData']['results'][0]['content']).replace("\n", "").replace("<b>", "").replace("</b>", "")
+			title = parser.unescape(json['responseData']['results'][0]['title']).replace("<b>", "").replace("</b>", "")
+			url = json['responseData']['results'][0]['unescapedUrl']
+			send_data("PRIVMSG %s :%s - %s - %s" % (variables.channel, title, content, url))
+		except:
+			send_data("PRIVMSG %s :Error, you should probably not do that again." % variables.channel)
