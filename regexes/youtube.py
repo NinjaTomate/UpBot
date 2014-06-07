f14c425a7b5d12887fcb2dcf82e1113443f9d4b3
NinjaTomate <ninjatomate@gmail.com>
2014-06-07 22:28:25 +0200

    Improved YouTube URL detection

--- regexes/youtube.py
+++ regexes/youtube.py
@@ -1,16 +1,15 @@
+#coding: utf-8
 import string, json as simplejson, urllib, re, variables
 def setup():
-	return "if re.match(r'(.*)https?://(?:www\.)?youtube', msg):\
+	return "if re.match(r'http(s)?://(www\.)?youtu(\.)?be(\..*/)?(watch)?.*', msg):\
 				youtube.execute(msg, send_data)"
 def execute(msg, send_data):
-	try:
-		yURL = string.split(string.split(re.split('(&|\?)v=', msg)[2], '&')[0])[0]
-  		url='http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2' % yURL
-  		json = simplejson.load(urllib.urlopen(url))
-  		title = json['entry']['title']['$t']
-  		author = json['entry']['author'][0]['name']['$t']
-  		send_data("PRIVMSG %s :YouTube: %s - Uploaded by %s" % (variables.channel, title, author))
-  		print "Youtube URL: %s" % yURL
-  		print "%s by %s - %s" % (title, author, url)
-  	except:
-  		print "Oopsie, youtube.py broke."
+  url = re.search(r'http(s)?://(www\.)?youtu(\.)?be(\..*/)?(watch)?.*', msg).group(0)
+  yURL = re.split(r'((\?|&)?(v=|\.be/)|&)', url)[4]
+  url='http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2' % yURL
+  json = simplejson.load(urllib.urlopen(url))
+  title = json['entry']['title']['$t']
+  author = json['entry']['author'][0]['name']['$t']
+  send_data("PRIVMSG %s :YouTube: %s - Uploaded by %s" % (variables.channel, title, author))
+  print "Youtube URL: %s" % yURL
+  print "%s by %s - %s" % (title, author, url)
