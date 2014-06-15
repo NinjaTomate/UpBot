6b777cad2bb6111215e8eb33249237043e6ce8e2
Spacedude <admin@spaaaceco.de>
2014-06-16 01:05:33 +0200

    Updated wp and np

--- modules/np.py
+++ modules/np.py
@@ -1,36 +1,63 @@
 import variables, urllib, json as simplejson, string, HTMLParser
-def np(send_data, msgarr, user):
-	username = ""
-	if len(msgarr) < 2:
-		lastfm = simplejson.load(open("lastfm.json", "r"))
-		if user in lastfm:
-		    username = lastfm[user]
-		    listener = user
-		else:
-		    send_data("PRIVMSG %s :No or invalid search string supplied." % variables.channel)
-	else:
-		username = string.join(msgarr)[4:]
-		listener = username
-	if username != "":
-		query = username
-		try:
-			parser = HTMLParser.HTMLParser()
-			url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&limit=1&format=json" % (query, variables.lastFMKey)
-			print query
-			json = simplejson.load(urllib.urlopen(url))
-			print json
-			try:
-				artist = parser.unescape(json['recenttracks']['track'][0]['artist']['#text'])
-				title = parser.unescape(json['recenttracks']['track'][0]['name'])
-				playstatus = "is"
-			except:
-				artist = parser.unescape(json['recenttracks']['track']['artist']['#text'])
-				title = parser.unescape(json['recenttracks']['track']['name'])
-				playstatus = "was"
 
-			send_data("PRIVMSG %s :%s %s playing: %s - %s" % (variables.channel, listener, playstatus, artist, title))
-		except Exception as e:
-			print e
-			send_data("PRIVMSG %s :%s" % (variables.channel, "I just broke. Pester Spacecode about it."))
+def np(send_data, msgarr, user):
+    username = ""
+
+    if len(msgarr) < 2:
+        lastfm = simplejson.load(open("lastfm.json", "r"))
+
+        if user in lastfm:
+            username = lastfm[user]
+            listener = user
+        else:
+            send_data("PRIVMSG %s :No or invalid search string supplied." % variables.channel)
+    else:
+        username = string.join(msgarr)[4:]
+        listener = username
+
+    if username != "":
+        query = username
+
+        try:
+            parser = HTMLParser.HTMLParser()
+            url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&limit=1&format=json" % (query, variables.lastFMKey)
+
+            variables.debug(query, 3)
+            json = simplejson.load(urllib.urlopen(url))
+            variables.debug(json, 3)
+
+            try:
+                artist = parser.unescape(json['recenttracks']['track'][0]['artist']['#text'])
+                title = parser.unescape(json['recenttracks']['track'][0]['name'])
+                playstatus = "is"
+            except:
+                artist = parser.unescape(json['recenttracks']['track']['artist']['#text'])
+                title = parser.unescape(json['recenttracks']['track']['name'])
+                playstatus = "was"
+
+            tagurl = "http://ws.audioscrobbler.com/2.0/?method=track.getTopTags&artist=%s&track=%s&api_key=%s&format=json" % (artist, title, variables.lastFMKey)
+            artisttagurl = "http://ws.audioscrobbler.com/2.0/?method=artist.getTopTags&artist=%s&api_key=%s&format=json" % (artist, variables.lastFMKey)
+
+            try:
+                jsontag = simplejson.load(urllib.urlopen(tagurl))
+                toptags = "("\
+                        + parser.unescape(jsontag['toptags']['tag'][0]['name']) + ", "\
+                        + parser.unescape(jsontag['toptags']['tag'][1]['name']) + ", "\
+                        + parser.unescape(jsontag['toptags']['tag'][2]['name']) + ")"
+            except:
+                try:
+                    jsonartisttag = simplejson.load(urllib.urlopen(artisttagurl))
+                    toptags = "("\
+                            + parser.unescape(jsonartisttag['toptags']['tag'][0]['name']) + ", "\
+                            + parser.unescape(jsonartisttag['toptags']['tag'][1]['name']) + ", "\
+                            + parser.unescape(jsonartisttag['toptags']['tag'][2]['name']) + ")"
+                except:
+                    toptags = ""
+
+            send_data("PRIVMSG %s :%s %s playing: %s - %s %s" % (variables.channel, listener, playstatus, artist, title, toptags))
+        except Exception as e:
+            print e
+            send_data("PRIVMSG %s :%s" % (variables.channel, "I just broke. Pester Spacecode about it."))
+
 def help(send_data):
-	send_data("PRIVMSG %s :Displays user's currently playing song. Usage: .np [lastfm username (optional)]"  % variables.channel)
+    send_data("PRIVMSG %s :Displays user's currently playing song. Usage: .np [lastfm username (optional)]"  % variables.channel)
--- modules/wp.py
+++ modules/wp.py
@@ -1,28 +1,57 @@
-import variables, re, string, np, json, urllib
+import variables, re, string, json as simplejson, urllib, HTMLParser
 from time import sleep
+
 def wp(send_data, msgarr, user):
-	lastfm = json.load(open("lastfm.json", "r"))
-	done = False
-	send_data("NAMES %s" % variables.channel)
-	while not done:
-		if "353" in variables.buffer or "stop" in variables.buffer:
-			input = variables.buffer
-			done = True
-	names = string.split(input)[6:]
-	for name in names:
-		if re.match(r'^(\@|\&|%|\+).*', name):
-			name = name[1:]
-		print name
-		if name in lastfm:
-			print lastfm [name]
-			url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&limit=1&format=json" % (lastfm[name], variables.lastFMKey)
-			lfmjson = json.load(urllib.urlopen(url))
-			try:
-				artist = lfmjson['recenttracks']['track'][0]['artist']['#text']
-				title = lfmjson['recenttracks']['track'][0]['name']
-				playstatus = "is"
-			except:
-				artist = lfmjson['recenttracks']['track']['artist']['#text']
-				title = lfmjson['recenttracks']['track']['name']
-				playstatus = "was"
-			send_data("PRIVMSG %s :%s %s playing: %s - %s" % (variables.channel, name, playstatus, artist, title))
+    lastfm = simplejson.load(open("lastfm.json", "r"))
+    done = False
+    send_data("NAMES %s" % variables.channel)
+
+    while not done:
+        if "353" in variables.buffer or "stop" in variables.buffer:
+            input = variables.buffer
+            done = True
+
+    names = string.split(input)[6:]
+    parser = HTMLParser.HTMLParser()
+
+    for name in names:
+        if re.match(r'^(\@|\&|%|\+).*', name):
+            name = name[1:]
+
+        variables.debug(name, 3)
+
+        if name in lastfm:
+            variables.debug(lastfm[name], 3)
+
+            url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&limit=1&format=json" % (lastfm[name], variables.lastFMKey)
+            json = simplejson.load(urllib.urlopen(url))
+
+            try:
+                artist = json['recenttracks']['track'][0]['artist']['#text']
+                title = json['recenttracks']['track'][0]['name']
+                playstatus = "is"
+            except:
+                artist = json['recenttracks']['track']['artist']['#text']
+                title = json['recenttracks']['track']['name']
+                playstatus = "was"
+
+            tagsurl = "http://ws.audioscrobbler.com/2.0/?method=track.getTopTags&artist=%s&track=%s&api_key=%s&format=json" % (artist, title, variables.lastFMKey)
+            artisttagsurl = "http://ws.audioscrobbler.com/2.0/?method=artist.getTopTags&artist=%s&api_key=%s&format=json" % (artist, variables.lastFMKey)
+
+            try:
+                jsontags = simplejson.load(urllib.urlopen(tagsurl))
+                toptags = "("\
+                        + parser.unescape(jsontags['toptags']['tag'][0]['name']) + ", "\
+                        + parser.unescape(jsontags['toptags']['tag'][1]['name']) + ", "\
+                        + parser.unescape(jsontags['toptags']['tag'][2]['name']) + ")"
+            except:
+                try:
+                    jsonartisttags = simplejson.load(urllib.urlopen(artisttagsurl))
+                    toptags = "("\
+                            + parser.unescape(jsonartisttags['toptags']['tag'][0]['name']) + ", "\
+                            + parser.unescape(jsonartisttags['toptags']['tag'][1]['name']) + ", "\
+                            + parser.unescape(jsonartisttags['toptags']['tag'][2]['name']) + ")"
+                except:
+                    toptags = ""
+
+            send_data("PRIVMSG %s :%s %s playing: %s - %s %s" % (variables.channel, name, playstatus, artist, title, toptags))
