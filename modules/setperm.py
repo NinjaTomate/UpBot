33a6a02caef0dcb744c2757960215f37f0945fd4
NinjaTomate <ninjatomate@gmail.com>
2014-06-19 23:08:21 +0200

    Updated setperm module
    Now allows people with premission level >800 to change others'
    permission level

--- modules/setperm.py
+++ modules/setperm.py
@@ -1,17 +1,19 @@
 import variables, os, string, json
 def setperm(send_data, msgarr, user, perms):
+	usrperms = 0
+	target = ""
 	if not len(msgarr) == 3:
 		send_data("PRIVMSG %s :Insufficient variables supplied." % variables.channel)
-	elif int(perms) < 999:
-		send_data("NOTICE %s :Check your privilege." % user)
+	elif int(perms) < 800 or msgarr[1] == variables.owner or int(msgarr[2]) >= int(perms):
+			send_data("NOTICE %s :Check your privilege." % user)
 	else:
 		target = msgarr[1]
-		perms = msgarr[2]
+		usrperms = int(msgarr[2])
 		accounts = json.load(open("accounts.json", 'r'))
 		accfile = open("accounts.json", 'w')
 		if not target in accounts:
 			accounts[target] = {}
-        accounts[target]['perms'] = perms
-        json.dump(accounts, accfile)
-        accfile.close()
-        send_data("NOTICE %s :Successfully changed permissions of %s to %s" % (user, target, perms))
+		accounts[target]['perms'] = usrperms
+		json.dump(accounts, accfile)
+		accfile.close()
+		send_data("NOTICE %s :Successfully changed permissions of %s to %s" % (user, target, usprerms))
